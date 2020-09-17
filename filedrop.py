074fd3d89841e384b8e68fa87186859e15acb805
coobster <57525815+coobster@users.noreply.github.com>
2020-09-17 02:00:42 -0700

    Add files via upload

--- filedrop.py
+++ filedrop.py
@@ -1,8 +1,12 @@
-from flask import Flask,g,send_file,abort,request
+from flask import Flask,g,send_file,abort,request,render_template
+from werkzeug.utils import secure_filename
 from sqlite3 import connect
 from hashlib import sha256
+from base64 import urlsafe_b64encode
+
 from time import time,sleep
 from io import BytesIO
+from os.path import join
 from threading import Thread
 
 app = Flask(__name__)
@@ -17,34 +21,27 @@ def get_db():
 
 def hash_file(data):
   tmp_data = str(data).encode('utf-8')
-  return sha256(tmp_data).hexdigest()
+  return sha256(tmp_data).digest()
+
+def encode_url(data):
+	return urlsafe_b64encode(data).decode()
 
 def cleanup_robot():
 	while True:
 		db = connect(DATABASE)
 		cur = db.cursor()
-		# select files older than 24 hours
-		results = cur.execute("SELECT url FROM links WHERE timestamp < ?",(time()-(86400),))
+		# delete anything over 24 hours old
+		results = cur.execute("SELECT url FROM links WHERE timestamp < ? AND url",(time()-86400,))
 		for row in results.fetchall():
-			cur.execute("DELETE FROM links WHERE url=?",(row[0],))
-			print('Robot deleted: {}'.format(row[0]))
+			cur.execute("UPDATE links SET data='',url='' WHERE url=?",(row[0],))
+			print('Robot erased: {}'.format(row[0]))
 		db.commit()
 		db.close()
-		# run again in one hour
-		sleep(3600)
+		sleep(1800) #run every 30 minutes
 
 @app.route('/')
 def index():
-  html = """
-
-Upload your file and get a single use URL download for 24 hours:
-<form action="/upload" method="POST" enctype="multipart/form-data">
-<input type="file" name="file">
-<br><input type="submit" value="Submit File">
-
-
-"""
-  return html
+  return render_template('index.html')
 
 @app.route('/upload',methods=["GET","POST"])
 def upload():
@@ -55,33 +52,31 @@ def upload():
 		filename = secure_filename(uploaded_file.filename)
 		ext = filename.split('.')[-1]
 		process_stamp = time()
-		url = hash_file(str(process_stamp)+str(data_hash))
+		url = encode_url(hash_file(str(process_stamp)+str(data_hash)))
 
 		if data != '':
 			cur = get_db().cursor()
-			cur.execute('INSERT INTO links VALUES(?,?,?,?,?)',(url,data_hash,data,process_stamp,ext))
+			cur.execute('INSERT INTO links VALUES(?,?,?,?,?,?,?)',(url,data_hash,data,process_stamp,ext,filename,request.remote_addr))
 			get_db().commit()
 			del cur
-			return "{}".format(url)
+			return "{}{}".format(request.host_url,url)
 		else:
-			return "ERROR 403"
+			return abort(404)
 	else:
-		return 'ERROR 404'
-@app.route('/<filename>')
-def get_file(filename):
+		return abort(404)
+@app.route('/<url>')
+def get_file(url):
 	cur = get_db().cursor()
-	results = cur.execute("SELECT data,ext FROM links WHERE url=?",(filename,))
+	results = cur.execute("SELECT data,ext FROM links WHERE url=?",(url,))
 	data = results.fetchone() 
 	if data:
-		cur.execute('DELETE FROM links WHERE url=?',(filename,))
+		cur.execute("UPDATE links SET url='',data='' WHERE url=?",(url,))
 		get_db().commit()
 		return send_file(BytesIO(data[0]),mimetype=data[1])
 	else:
-		return "ERROR 404"
+		return abort(404)
 
 if __name__ == '__main__':
-	# start the cleaning robot to remove files older than 24 hours
 	bot = Thread(target=cleanup_robot,args=())
 	bot.start()
-	# start the flask app
 	app.run(host='0.0.0.0',port=5000)
